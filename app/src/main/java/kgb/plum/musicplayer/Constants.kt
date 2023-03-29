c0ef3a68fad078a8d2ecff362296f4266290d58c
agfalcon <agfalcon12@gmail.com>
2023-03-29 12:37:42 +0900

    MediaPlayer Background 실행 구현 완료

---
+++ app/src/main/java/kgb/plum/musicplayer/Constants.kt
@@ -0,0 +1,5 @@
+package kgb.plum.musicplayer
+
+const val MEDIA_PLAYER_PLAY = "play"
+const val MEDIA_PLAYER_PAUSE = "pause"
+const val MEDIA_PLAYER_STOP = "stop"
--- app/src/main/java/kgb/plum/musicplayer/MainActivity.kt
+++ app/src/main/java/kgb/plum/musicplayer/MainActivity.kt
@@ -1,11 +1,41 @@
 package kgb.plum.musicplayer
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kgb.plum.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener{ mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerPlay() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply{ action = MEDIA_PLAYER_PLAY }
+        startService(intent)
+    }
+
+    private fun mediaPlayerPause() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply{ action = MEDIA_PLAYER_PAUSE }
+        startService(intent)
+    }
+
+    private fun mediaPlayerStop() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply{ action = MEDIA_PLAYER_STOP }
+        startService(intent)
     }
 }
